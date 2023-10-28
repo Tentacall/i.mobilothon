e1adf449b559372b84bf1695c165012674632b64
Rupak Biswas <rupak_2001cs57@iitp.ac.in>
2023-10-28 18:02:42 +0530

    [+] minor update

--- protocol/proto_py/main.test.py
+++ protocol/proto_py/main.test.py
@@ -4,8 +4,10 @@ from proto import CProto
 if __name__ == "__main__":
     proto = CProto()
     # proto.show()
+    print(">>> [method] [retain] [auth] [dtype] [topic] [...msg]")
     while True:
         token = input(">>> ").split()
+        # example: 
         if len(token) == 0:
             break
         
@@ -14,7 +16,7 @@ if __name__ == "__main__":
         auth = bool(int(token[2]))
         dtype = int(token[3])
         topic = int(token[4])
-        msg = " ".join(token[5:])
+        msg = " ".join(token[5:]) if len(token) > 5  else None
 
         proto.send(method, retain, auth, dtype, topic, msg)
         
