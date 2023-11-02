e54b14366a0e70c5f4bb0bd11acaaf0c32e70032
Rupak Biswas <rupak_2001cs57@iitp.ac.in>
2023-11-02 16:26:50 +0530

    [?] Threading problem resolved

--- client/client.py
+++ client/client.py
@@ -1,16 +1,15 @@
-from protocol.proto_py.proto import CProto, PearsonHashing
+import queue
+import threading
+from scapy.all import *
+
+from broker.loggings import logger
 from protocol.proto_py.utils import DtypeParser
 from protocol.proto_py.standards import Method, DType
+from protocol.proto_py.proto import CProto, PearsonHashing
 
-import threading
-import queue
 
 class Client:
     def __init__(self, src, dst, sport, dport, client_id = 0) -> None:
-        self.src = src
-        self.dst = dst
-        self.sport = sport
-        self.dport = dport
         self.client_id = client_id
         self.proto = CProto(src, dst, sport, dport)
         self.dtype_parser = DtypeParser()
@@ -119,11 +118,50 @@ class Client:
             dtype = int(token[3])
             topic = int(token[4])
             msg = " ".join(token[5:]) if len(token) > 5  else None
-            
+
+    def callback(self, pkt):
+        pass
+
+    def start_listener(self):
+        def listener_thread_func():
+            print(f"Listening on port {self.sport}")
+            sniff(filter="tcp", prn=self.callback)
+
+        self.listener_thread = threading.Thread(target=listener_thread_func)
+        self.listener_thread.daemon = True
+        self.listener_thread.start()
+
+
+class MethodHandler:
+    def __init__(self):
+        self.method_handlers = [None] * 256
+        self.dtype_parser = DtypeParser()
+        self.hashing = PearsonHashing()
+
+        self.__init__basic_method()
+
+    def __call__(self, method, auth, dtype, topic, data, dst_ip, dst_port):
+        data = self.dtype_parser.decode(dtype, data)
+        self.sender.set_dst(dst_ip, dst_port)
+        return self.method_handlers[method](data, auth, dtype, topic, dst_ip, dst_port)
+
+    def _set_permutation(self, T):
+        self.hashing.T = T
+
+    def __init__basic_method(self):
+        # 0x00
+        def ping(*args):
+            logger.info(f"Ping from {args[4]}:{args[5]}")
+            # send a pong message
+            self.sender.send(Method.Pong.value, 0x0, 0x0, DType.Null.value, 0x00)
+
+        self.method_handlers[Method.Ping.value] = ping
+
+        # 0x01
+        self.method_handlers[Method.Pong.value] = lambda *args: logger.info(f"Pong from {args[4]}:{args[5]}")
+
+
 if __name__ == "__main__":
-    import time
-    client = Client("10.35.0.93", "10.35.0.93", 9779, 9779)
-    client.connect()
-    time.sleep(10)
-    client.disconnect()
-    
+    client = Client("10.35.0.93", "10.38.0.242", 9779, 9779)
+    client.start_listener()
+    client.cli()
--- client/clientA.py
+++ client/clientA.py
@@ -6,28 +6,28 @@ class ClientA(Client):
         self.sport = sport
         super().__init__(src, dst, sport, dport)
 
-    def connect(self, ip, port):
-        self.proto.set_dst(ip, port)
-        self.proto.send(Method.CONNECT, False, False, DType.INT, 0, self.sport)
+    # def connect(self, ip, port):
+    #     self.proto.set_dst(ip, port)
+    #     self.proto.send(Method.CONNECT, False, False, DType.INT, 0, self.sport)
     
-    def cli(self):
-        print(f"[ Client A ]: Starting at port {self.sport}")
-        print(">>> [method] [retain] [auth] [dtype] [topic] [...msg]")
+    # def cli(self):
+    #     print(f"[ Client A ]: Starting at port {self.sport}")
+    #     print(">>> [method] [retain] [auth] [dtype] [topic] [...msg]")
 
-        while True:
-            token = input(">>> ").split()
-            # example: 
-            if len(token) == 0:
-                break
+    #     while True:
+    #         token = input(">>> ").split()
+    #         # example: 
+    #         if len(token) == 0:
+    #             break
             
-            method = int(token[0])
-            retain = bool(int(token[1]))
-            auth = bool(int(token[2]))
-            dtype = int(token[3])
-            topic = int(token[4])
-            msg = " ".join(token[5:]) if len(token) > 5  else None
+    #         method = int(token[0])
+    #         retain = bool(int(token[1]))
+    #         auth = bool(int(token[2]))
+    #         dtype = int(token[3])
+    #         topic = int(token[4])
+    #         msg = " ".join(token[5:]) if len(token) > 5  else None
 
-            self.proto.send(method, retain, auth, dtype, topic, msg)
+    #         self.proto.send(method, retain, auth, dtype, topic, msg)
 
 
 
